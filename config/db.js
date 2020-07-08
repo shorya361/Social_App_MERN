e0219b7a88b70d2e3272dd125bad2703df263111
shorya361 <55752959+shorya361@users.noreply.github.com>
2020-07-08 22:18:08 +0530

    setting up initial routes and MongoDB Atlas connection

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    console.log('connecting Mongo-Atlas');
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Atlas Connected');
+  } catch (error) {
+    console.log(error.message);
+    //Exit Process with status failure code 1
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+// connect DB
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API Running!!!'));
 app.listen(PORT, () => {
   console.log('Server started at port ' + PORT);
 });
+
+// Define Route
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
