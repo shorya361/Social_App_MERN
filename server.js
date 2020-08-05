6f9404bb8875765dea4274fe06c4cebc9c857644
shorya361 <55752959+shorya361@users.noreply.github.com>
2020-08-06 00:59:27 +0530

    prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 // connect DB
 connectDB();
 
@@ -10,7 +11,8 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('API Running!!!'));
+// app.get('/', (req, res) => res.send('API Running!!!'));
+
 app.listen(PORT, () => {
   console.log('Server started at port ' + PORT);
 });
@@ -20,3 +22,11 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/comment', require('./routes/api/comments'));
 app.use('/api/Posts', require('./routes/api/Posts'));
+
+//Serve static assets in productions
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
